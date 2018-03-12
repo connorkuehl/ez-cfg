4510086e59e1284ec4aa71ff5e89c2550255628f
Connor Kuehl <cipkuehl@gmail.com>
2018-03-11 21:14:16 -0700

    Initial implementation of lexer.

---
+++ lexer.cpp
@@ -0,0 +1,11 @@
+#include "lexer.h"
+
+#include <fstream>
+#include <iterator>
+
+std::vector<lexeme>&& lex(const std::string& file)
+{
+    std::ifstream unlexed(file);
+    std::vector<lexeme> tokens (std::istream_iterator<std::string>{unlexed}, {});
+    return std::move(tokens);
+}
---
+++ lexer.h
@@ -0,0 +1,11 @@
+#ifndef LEXER_H_
+#define LEXER_H_
+
+#include <vector>
+#include <string>
+
+typedef std::string lexeme;
+
+std::vector<lexeme>&& lex(const std::string& file);
+
+#endif

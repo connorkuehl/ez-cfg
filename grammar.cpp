300f9b09a0e77c299c99edb5244142b71faca8f3
Connor Kuehl <cipkuehl@gmail.com>
2018-03-12 20:45:02 -0700

    Refactor parse and display. Add getter for start symbol.

--- grammar.cpp
+++ grammar.cpp
@@ -8,6 +8,11 @@ grammar::grammar(const std::string& file)
     parse(lex(file));
 }
 
+const std::string& grammar::get_start() const
+{
+    return start;
+}
+
 bool grammar::is_terminal(const std::string& value) const
 {
     return !is_nonterminal(value);
@@ -23,7 +28,11 @@ void grammar::display() const
     for (const auto& rule : rules) {
         std::cout << rule.first << std::endl;
         for (const auto& builds : rule.second) {
-            std::cout << ":\t" << builds << std::endl;
+            std::cout << "\t";
+            for (const auto& prod : builds) {
+                std::cout << prod << " ";
+            }
+            std::cout << std::endl;
         }
         std::cout << std::endl;
     }
@@ -34,7 +43,8 @@ void grammar::parse(const std::vector<lexeme>& tokens)
     start = tokens.at(0);
 
     std::string curr_token;
-    std::vector<std::string> productions;
+    std::vector<std::vector<std::string>> productions;
+    std::vector<std::string> production;
     std::stringstream sentence;
     auto building{false};
 
@@ -50,11 +60,12 @@ void grammar::parse(const std::vector<lexeme>& tokens)
                         building = true;
                         curr_token = token;
                         productions.clear();
+                        production.clear();
                     } else {
                         // we are building a production rule, and
                         // we've just stumbled upon another production
                         // site.
-                        sentence << token << " ";
+                        production.push_back(token);
                     }
                 }
                 break;
@@ -65,27 +76,26 @@ void grammar::parse(const std::vector<lexeme>& tokens)
                     if (!building) {
                         // error
                     } else {
-                        productions.push_back(sentence.str());
-                        sentence.str("");
+                        productions.push_back(production);
+                        production.clear();
                     }
                 }
                 break;
             case ';': 
                 {
-                    productions.push_back(sentence.str());
-                    sentence.str("");
+                    productions.push_back(production);
                     insert_rule(curr_token, productions);
                     building = false;
                 }
                 break;
             default:
-                sentence << token << " ";
+                production.push_back(token);
                 break;
         }
     }
 }
 
-void grammar::insert_rule(const std::string& rule, const std::vector<std::string>& productions)
+void grammar::insert_rule(const std::string& rule, const std::vector<std::vector<std::string>>& productions)
 {
     auto found{rules.find(rule)};
     if (found == rules.end()) {
--- grammar.h
+++ grammar.h
@@ -11,15 +11,16 @@ class grammar final {
 public:
     grammar(const std::string& file);
 
+    const std::string& get_start() const;
     bool is_terminal(const std::string& value) const;
     bool is_nonterminal(const std::string& value) const;
     void display() const;
 private:
     std::string start;
-    std::map<std::string, std::vector<std::string>> rules;
+    std::map<std::string, std::vector<std::vector<std::string>>> rules;
 
     void parse(const std::vector<lexeme>& tokens);
-    void insert_rule(const std::string& rule, const std::vector<std::string>& productions);
+    void insert_rule(const std::string& rule, const std::vector<std::vector<std::string>>& productions);
 };
 
 #endif // GRAMMAR_H_
